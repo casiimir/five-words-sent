83b6fca9180c4e4195ae4083ec2b96389387434a
Casimiro <cp.ciancimino@gmail.com>
2021-02-22 20:12:37 +0100

    edit: the solution has been completed to all requests

--- script.js
+++ script.js
@@ -6,20 +6,16 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Type here your question:', (answer) => {
-  if (answer.length < 5) {
-    consola.error(`Please try again, ${answer} is too short...`);
-  } else {
-    consola.success(`Your answer: ${answer}`);
-    rl.close();
-  } 
-})
+function typeYourWords() {
+  rl.question('Type here your question:', (answer) => {
+    if (answer.length < 5) {
+      consola.error(new Error(`Please try again, '${answer}' is too short...`));
+      typeYourWords();
+    } else {
+      consola.success(`Your answer '${answer}' is valid!`);
+      rl.close();     
+    } 
+  })
+}
 
-
-
-// Creare uno script node che chiede all'utente di inserire una frase di almeno 5 parole da riga di comando.
-// Se la frase inserita ha meno di 5 parole viene stampato un messaggio di errore.
-// Altrimenti viene stampato un messaggio di successo.
-
-// Bonus (opzionale):
-// Lo script deve continuare a chiedere all'utente di inserire la frase finché non viene inserita una frase valida.
+typeYourWords();
